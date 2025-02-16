beb7aa906714800fc04af9939a6305706777aaeb
MateeJr <100082178+MateeJr@users.noreply.github.com>
2025-02-17 02:40:17 +0700

    Update client.ts

--- env/client.ts
+++ env/client.ts
@@ -3,7 +3,13 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const clientEnv = createEnv({
-  client: {},
-  runtimeEnv: {},
+  client: {
+    NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string(),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  },
 })
 
